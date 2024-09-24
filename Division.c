de29309745db26f7d40fedce5fd2b81a27eb0b6f
Fahad Anwar <officerops5@gmail.com>
2024-09-24 15:07:20 +0500

    Division.c

---
+++ Division.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+
+int x,y,division;
+printf("Enter the first number:\n");
+scanf("%d",&x);
+printf("Enter the second number:\n");
+scanf("%d",&y);
+
+division = x / y;
+
+
+if (x / y)
+{
+ printf("Answer = %d\n", division);
+}
+else
+{
+    printf("not divisible");
+}
+
+return 0;
+
+}
